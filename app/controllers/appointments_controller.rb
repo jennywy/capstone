3f0e302e0d2d26e55c4315acadb4e5664b2c5c74
jennywy <jennywy@gmail.com>
2017-11-01 16:30:22 -0400

    Appointments Controller Actions from Template
    Copied the Appointments controller from the Twilio docs template
    https://www.twilio.com/docs/tutorials/appointment-reminders-ruby-rails#interacting-with-appointments
    
    Commented out the flash

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,51 +1,80 @@
 class AppointmentsController < ApplicationController
-  before_action :set_appointment, only: [:show, :update, :destroy]
+  before_action :find_appointment, only: [:show, :edit, :update, :destroy]
 
   # GET /appointments
+  # GET /appointments.json
   def index
     @appointments = Appointment.all
-
-    render json: @appointments
+    # if @appointments.length.zero?
+    #   flash[:alert] = 'You have no appointments. Create one now to get started.'
+    # end
   end
 
   # GET /appointments/1
+  # GET /appointments/1.json
   def show
-    render json: @appointment
+  end
+
+  # GET /appointments/new
+  def new
+    @appointment = Appointment.new
+    @min_date = DateTime.now
+  end
+
+  # GET /appointments/1/edit
+  def edit
   end
 
   # POST /appointments
+  # POST /appointments.json
   def create
+    Time.zone = appointment_params[:time_zone]
     @appointment = Appointment.new(appointment_params)
 
-    if @appointment.save
-      render json: @appointment, status: :created, location: @appointment
-    else
-      render json: @appointment.errors, status: :unprocessable_entity
+    respond_to do |format|
+      if @appointment.save
+        format.html { redirect_to @appointment, notice: 'Appointment was successfully created.' }
+        format.json { render :show, status: :created, location: @appointment }
+      else
+        format.html { render :new }
+        format.json { render json: @appointment.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   # PATCH/PUT /appointments/1
+  # PATCH/PUT /appointments/1.json
   def update
-    if @appointment.update(appointment_params)
-      render json: @appointment
-    else
-      render json: @appointment.errors, status: :unprocessable_entity
+    respond_to do |format|
+      if @appointment.update(appointment_params)
+        format.html { redirect_to @appointment, notice: 'Appointment was successfully updated.' }
+        format.json { render :show, status: :ok, location: @appointment }
+      else
+        format.html { render :edit }
+        format.json { render json: @appointment.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   # DELETE /appointments/1
+  # DELETE /appointments/1.json
   def destroy
     @appointment.destroy
+    respond_to do |format|
+      format.html { redirect_to appointments_url, notice: 'Appointment was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_appointment
-      @appointment = Appointment.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  # See above ---> before_action :set_appointment, only: [:show, :edit, :update, :destroy]
+  def find_appointment
+    @appointment = Appointment.find(params[:id])
+  end
 
-    # Only allow a trusted parameter "white list" through.
-    def appointment_params
-      params.require(:appointment).permit(:name, :phone_number, :time)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def appointment_params
+    params.require(:appointment).permit(:name, :phone_number, :time, :time_zone)
+  end
 end
