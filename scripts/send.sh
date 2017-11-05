f4c5364e6554c8c55f5783036177690d81422e9d
jennywy <30730351+jennywy@users.noreply.github.com>
2017-11-05 09:51:20 -0500

    Delete send.sh

--- scripts/send.sh
+++
@@ -1,5 +0,0 @@
-curl 'https://api.twilio.com/2010-04-01/Accounts/AC8b747f9a427422ff9d136632a957d93b/Messages.json' -X POST \
---data-urlencode 'To=+16179998130' \
---data-urlencode 'From=+16179256628' \
---data-urlencode 'Body=Get out of my dreams and get into my car' \
--u AC8b747f9a427422ff9d136632a957d93b:84289e7a7ddbb43aa97d4936793c2bcc
