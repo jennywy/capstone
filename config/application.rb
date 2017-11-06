b6942a06def98f1a14a5cd646d31e193dde936e8
jennywy <jennywy@gmail.com>
2017-11-06 14:16:28 -0500

    Enable Delay Jobs (finally!)
    Configured to EST Time zone and to use delayed_jobs queue adapted
    Activate the delayed jobs adaptor by
    - adding DJ column,
    - running generating migration for DJ
    - and `bin/delayed_job start`
    
    Now able to use handle async method in the appointments model

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,9 @@ Bundler.require(*Rails.groups)
 module Capstone
   # :nodoc:
   class Application < Rails::Application
+    config.time_zone = 'Eastern Time (US & Canada)'
+    # config.active_record.raise_in_transactional_callbacks = true
+    # config.active_job.queue_adapter = :delayed_job
     # Settings in config/environments/* take precedence over those specified
     # here.
     # Application configuration should go into files in config/initializers
