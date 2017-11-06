0e28161597b0ef5231fd59a9a4a6f0213f183f1d
jennywy <jennywy@gmail.com>
2017-11-06 15:47:46 -0500

    Removing the queue adapter again

--- config/application.rb
+++ config/application.rb
@@ -22,8 +22,8 @@ module Capstone
   # :nodoc:
   class Application < Rails::Application
     config.time_zone = 'Eastern Time (US & Canada)'
-    config.active_record.raise_in_transactional_callbacks = true
-    config.active_job.queue_adapter = :delayed_job
+    # config.active_record.raise_in_transactional_callbacks = true
+    # config.active_job.queue_adapter = :delayed_job
     # Settings in config/environments/* take precedence over those specified
     # here.
     # Application configuration should go into files in config/initializers
